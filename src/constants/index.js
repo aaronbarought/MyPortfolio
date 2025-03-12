c8a578f72444a7e02c64e9ff8a3b635f66cd8cbc
aaronbarought <133719875+aaronbarought@users.noreply.github.com>
2025-03-12 11:05:06 -0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I'm Aryan Barot, a full-stack developer and Computer Science student at Arizona State University. With experience in software engineering and data operations, I specialize in Python, JavaScript, React, Node.js, and SQL. I've worked on optimizing web applications, automating workflows, and building scalable solutions. Explore my portfolio to see my experience, projects, and let's connect`;
+export const HERO_CONTENT = `I'm Aryan Barot, a full-stack developer and Computer Science student at Arizona State University. With experience in software engineering and data operations, I specialize in Python, C++, C#, AWS, Firebase, JavaScript, React, Node.js, and SQL. I've worked on optimizing web applications, automating workflows, and building scalable solutions. Explore my portfolio to see my experience, projects, and let's connect`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
